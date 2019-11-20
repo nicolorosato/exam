4411b6e264b268bdc3781974e793eaa900b64879
Nicolò Rosato <nicolorosato@NicoloRosato.local>
2019-11-20 16:58:11 +0100

    Comment the file main.py

--- myfolder/main.py
+++ myfolder/main.py
@@ -1,5 +1,9 @@
 #! /usr/bin/env python3
 
+'''
+From module Capitals we import both the key and the value in order to make available the function.
+'''
+
 from capitals import check_capital, check_state
 
 check_capital("Germany")
