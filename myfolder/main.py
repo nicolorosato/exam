8d959fe8b729521a46efa99616f00aeaf73ae66f
comar-lab <marco.piccolo@student.h-farm.com>
2019-11-20 11:17:03 -0500

    PEP8 main.py

--- myfolder/main.py
+++ myfolder/main.py
@@ -1,15 +1,19 @@
 #! /usr/bin/env python3
 
-'''importing sys'''
-
-import sys
+import sys '''Importing sys module '''
 
 '''
-From module Capitals we import both the key and the value in order to make available the function.
+From module Capitals we import both the key,
+
+the value in order to make available the function.
 '''
 
 from capitals import check_capital, check_state
 
+'''Allow user to input from the terminal,
+
+2 arguments.
+'''
+
 check_capital(sys.argv[1])
 check_state(sys.argv[2])
-
