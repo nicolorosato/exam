dccc304f99a759099dd931bcbde5320a71d9c87f
Paulk97 <paolo.schiavulli@gmail.com>
2019-12-03 13:17:15 -0500

    argparse

--- myfolder/main.py
+++ myfolder/main.py
@@ -1,19 +1,75 @@
 #! /usr/bin/env python3
 
-import sys '''Importing sys module '''
+''' importing related modules
 
 '''
-From module Capitals we import both the key,
 
-the value in order to make available the function.
+
+import argparse
+import csv
+import time
+import sys
+from capitals import check_capital
+
+
+'''Implementing argparse position and optional argument
+
 '''
 
-from capitals import check_capital, check_state
 
-'''Allow user to input from the terminal,
+parser = argparse.ArgumentParser()
+parser.add_argument('state', type=str, help="Dispaly capital's state name")
+parser.add_argument('-v', '--verbosity',
+                    help='increase verbosity', action='count', default=0)
+args = parser.parse_args()
+variable = check_capital(args.state)
+# print (args.state)
 
-2 arguments.
+
+'''Defining levels of verbosity
+
 '''
 
-check_capital(sys.argv[1])
-check_state(sys.argv[2])
+
+if args.verbosity >= 2:
+    time.sleep(1)
+    print('Running {}...'.format(__file__))
+    for i in range(101):
+        time.sleep(0.02)
+        sys.stdout.write("\r%d%%" % i)
+        sys.stdout.flush()
+    print(' ')
+    time.sleep(1)
+    print('Extracting your input from csv file...')
+    for i in range(101):
+        time.sleep(0.02)
+        sys.stdout.write("\r%d%%" % i)
+        sys.stdout.flush()
+    print('')
+    time.sleep(1)
+    print('Fiding matching value...')
+    for i in range(101):
+        time.sleep(0.02)
+        sys.stdout.write("\r%d%%" % i)
+        sys.stdout.flush()
+    print('')
+    time.sleep(1)
+    print('Obtainng the name of the capital...')
+    for i in range(101):
+        time.sleep(0.02)
+        sys.stdout.write("\r%d%%" % i)
+        sys.stdout.flush()
+    print('')
+    time.sleep(1)
+    print('Returning matched value...')
+    for i in range(101):
+        time.sleep(0.02)
+        sys.stdout.write("\r%d%%" % i)
+        sys.stdout.flush()
+    print('')
+    time.sleep(2)
+    print("The Eu state's capital of {} is {}".format(args.state, variable))
+elif args.verbosity >= 1:
+    print("The Eu state's capital of {} is {}".format(args.state, variable))
+else:
+    print(variable)
