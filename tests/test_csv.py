70ce89936d75cd9b79bfaee24c9183c264eaa826
Nicolò Rosato <nicolorosato@NicoloRosato.local>
2019-12-14 19:28:39 +0100

    test_csv.py

--- tests/test_csv.py
+++ tests/test_csv.py
@@ -12,36 +12,36 @@ sys.path.append('/Users/nicolorosato/capitals/data/capitals.csv')
 
 
 class TestMain(unittest.TestCase):
-
+    
     def setUp(self):
         self.temporary_file = "/tmp/temporary_file.csv"
         f = open(self.temporary_file, 'w')
         f.close()
 
 
-'''This function is necessary to check
+    '''This function is necessary to check
 
-the existence of the csv file.
-'''
+    the existence of the csv file.
+    '''
 
     def test_no_datafile(self):
         datafile = csv_reader(path=self.temporary_file)
         self.assertIn(".csv", self.temporary_file)
 
-'''This function is necessary to check 
+    '''This function is necessary to check 
 
-the presence of data inside the csv file.
-'''
+    the presence of data inside the csv file.
+    '''
 
     def test_empty_datafile(self):
         datafile = csv_reader(path=self.temporary_file)
         self.assertFalse(datafile)
 
-'''This function is necessary to check 
+    '''This function is necessary to check 
 
-the extension of the file.
+    the extension of the file.
 
-'''       
+    '''       
     def test_valid_extension(self):
         extension = type_file(path=self.temporary_file)
         self.assertEqual(extension, ".csv")
